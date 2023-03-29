fa1d546a4ac408ea941ff74e25ac2009b41e93e5
mikerogers04 <mikerogers04@hotmail.com>
2023-03-29 15:51:42 -0700

    Added index controller, route, and view for index page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+ #HTTP verb, url(location), hash rocket, controller, method
+ get '/blogs' => 'blog#index', as: 'blogs'
 end
