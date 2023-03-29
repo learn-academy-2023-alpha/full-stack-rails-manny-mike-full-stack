2325f62584548d96834e7cfaa69a9445a8022f11
Manflo27 <maflores1987@yahoo.com>
2023-03-29 16:20:19 -0700

    added show, controller, route and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
  #HTTP verb, url(location), hash rocket, controller, method
  get '/blogs' => 'blog#index', as: 'blogs'
+ get '/blogs/:id' => 'blog#show', as: 'blog'
 end
