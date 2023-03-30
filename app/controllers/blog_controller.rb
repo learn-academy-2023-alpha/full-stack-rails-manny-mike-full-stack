7212a9cedebfd992b1961efcd40edc6cbbb0c77c
mikerogers04 <mikerogers04@hotmail.com>
2023-03-30 09:42:22 -0700

    Added new controller, route, view for blog application.

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -3,7 +3,13 @@ class BlogController < ApplicationController
     def index
         @blogs = Blog.all
     end
+
     def show
         @blog = Blog.find(params[:id])
     end
+
+    def new
+        @blog = Blog.new
+    end
+
 end
